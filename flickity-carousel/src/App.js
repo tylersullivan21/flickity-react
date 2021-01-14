af0adfb752e4065a2666bcea6cf6e138694d23d6
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2021-01-14 16:57:32 -0500

    Last push of the day

--- flickity-carousel/src/App.js
+++ flickity-carousel/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Carousel from './Components/Carousel/index'
+import ReactDOM from "react-dom";
+import Flickity from "react-flickity-component";
+
+import "../src/flickity.css";
+import "../src/index.css";
+
+function Carousel() {
+  return (
+    <Flickity>
+      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
+      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
+      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
+      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
+    </Flickity>
+  );
+}
+
 function Home() {
   return (
     <div className="App">
-       <Carousel />
       <h1>React Flickity</h1>
-     
+      <Carousel />
     </div>
   );
 }
--- flickity-carousel/src/Components/Carousel/index.js
+++ flickity-carousel/src/Components/Carousel/index.js
@@ -1,19 +1,52 @@
-import ReactDOM from "react-dom";
+import React from "react";
+import $ from "jquery";
 import Flickity from "react-flickity-component";
-
-import "../src/flickity.css";
-import "../src/index.css";
-
-function Carousel() {
-  return (
-    <Flickity>
-      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
-      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
-      <img style={{ marginLeft: 0, marginRight: 0 }} src="https://placeimg.com/640/480/animals" />
-      <img style={{ marginLeft: 5, marginRight: 5 }} src="https://placeimg.com/640/480/animals" />
-    </Flickity>
-  );
+class Carousel extends React.Component{
+componentDidMount(){
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity( elem, {
+      // options
+      cellAlign: 'center',
+      contain: false,
+    
+    });
+    
+    // element argument can be a selector string
+    //   for an individual element
+    var flkty = new Flickity( '.carousel', {
+      // options
+      wrapAround: true,
+      freeScroll: true,
+freeScrollFriction: 0.03
+    });
 }
 
+render(){
+    return(
+    <React.Fragment>
+ 
+ <div class="carousel">
+  <div class="carousel-cell">
+  <img src="https://placeimg.com/640/480/animals" alt=""/>
+  </div>
+  <div class="carousel-cell">
+  <img src="https://placeimg.com/640/480/animals" alt=""/>
+  </div>
+  <div class="carousel-cell">
+  <img src="https://placeimg.com/640/480/animals" alt=""/>
+  </div>
+   <div class="carousel-cell">
+  <img src="https://placeimg.com/640/480/animals" alt=""/>
+  </div>
+
+</div>
+
+
+  </React.Fragment>
+    );
+}
+}
 
 export default Carousel;
+
+
